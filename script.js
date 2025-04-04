efa803b916c3b503e9dc31040582357188555075
Hector <hectorpena6590@gmail.com>
2025-04-03 21:42:33 -0700

    updating JS

--- script.js
+++ script.js
@@ -1,24 +1,37 @@
-
 async function fetchData() {
-    try {
-      let response = await fetch('https://www.omdbapi.com/?i=tt3896198&apikey=f9f11fce');
-      let data = await response.json();
-      console.log(data);
-    } catch (error) {
-      console.error('Error:', error);
-    }
-  }
-  
-  fetchData();
-
-  async function displayData() {
-    let response = await fetch('https://www.omdbapi.com/?i=tt3896198&apikey=f9f11fce');
+  try {
+    let response = await fetch(
+      "https://www.omdbapi.com/?i=tt3896198&apikey=f9f11fce"
+    );
     let data = await response.json();
-    
-    let container = document.getElementById('data-container');
-    container.innerHTML = data.map(item => `<p>${item.name}</p>`).join('');
+    console.log(data);
+  } catch (error) {
+    console.error("Error:", error);
+    return "Couldn't Fetch Data";
   }
-  
-  displayData();
+}
 
- 
+fetchData();
+
+async function displayData() {
+  try {
+    let response = await fetch(
+      "https://www.omdbapi.com/?i=tt3896198&apikey=f9f11fce"
+    );
+    let data = await response.json();
+
+    let container = document.getElementById("data-container");
+    container.innerHTML = `
+        <h2>${data.Title}</h2>
+        <p><strong>Year:</strong> ${data.Year}</p>
+        <p><strong>Rated:</strong> ${data.Rated}</p>
+        <p><strong></strong> <img src="https://m.media-amazon.com/images/M/MV5BNWE5MGI3MDctMmU5Ni00YzI2LWEzMTQtZGIyZDA5MzQzNDBhXkEyXkFqcGc@._V1_SX300.jpg" alt="Movie Poster"></p>
+          `;
+  } catch (error) {
+    console.error("Error:", error);
+    return "Couldn't Fetch Data";
+  }
+}
+
+displayData();
+
