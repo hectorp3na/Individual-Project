cf3cf7f7493598e1051eb643702cbb3f23489c50
Hector <hectorpena6590@gmail.com>
2025-04-06 23:04:13 -0700

    adding a movie on the site

--- script.js
+++ script.js
@@ -17,16 +17,15 @@ fetchData();
 
 async function displayData() {
   try {
-    let response = await fetch(
+    const response = await fetch(
       "https://www.omdbapi.com/?i=tt3896198&apikey=f9f11fce"
     );
-    let data = await response.json();
+    const data = await response.json();
 
     let container = document.getElementById("data-container");
     container.innerHTML = `
         <h2>${data.Title}</h2>
-        <p><strong>Year:</strong> ${data.Year}</p>
-        <p><strong>Rated:</strong> ${data.Rated}</p>
+        <p><strong>Year:</strong>${data.Year}</p>
         <p><strong></strong> <img src="https://m.media-amazon.com/images/M/MV5BNWE5MGI3MDctMmU5Ni00YzI2LWEzMTQtZGIyZDA5MzQzNDBhXkEyXkFqcGc@._V1_SX300.jpg" alt="Movie Poster"></p>
           `;
   } catch (error) {
